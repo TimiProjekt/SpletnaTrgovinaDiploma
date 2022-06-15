3bc1f15135718afec7c21e0a3061287176190dfe
Tobias Mihelcic <Tobias.mihelcic@mendix.com>
2022-06-15 13:37:41 +0200

    Added show which category/home page is opened
    Added hot reload

---
+++ SpletnaTrgovinaDiploma/Helpers/ActiveLink.cs
@@ -0,0 +1,34 @@
+﻿namespace SpletnaTrgovinaDiploma.Helpers
+{
+    public static class ActiveLink
+    {
+        private static bool IsActiveCategory(string link, int id)
+        {
+            var splitLink = link.Split('/');
+
+            if (splitLink.Length == 4 && splitLink[1].ToLower() == "category")
+            {
+                var isParsed = int.TryParse(splitLink[3], out var number);
+                if (isParsed && number == id)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static string GetCssClassIfActiveCategory(string link, int id) => IsActiveCategory(link, id) ? "active-link" : "";
+
+        public static string GetChevronIfActiveCategory(string link, int id) => IsActiveCategory(link, id) ? ">" : "";
+
+        private static bool IsActiveHomePage(string link)
+        {
+            var splitLink = link.Split('/');
+
+            return splitLink.Length == 2 && splitLink[0].Length == 0 && splitLink[1].Length == 0;
+        }
+
+        public static string GetCssClassIfHomePage(string link) => IsActiveHomePage(link) ? "active-link" : "";
+
+        public static string GetChevronIfHomePage(string link) => IsActiveHomePage(link) ? ">" : "";
+    }
+}
--- SpletnaTrgovinaDiploma/Startup.cs
+++ SpletnaTrgovinaDiploma/Startup.cs
@@ -52,7 +52,8 @@ namespace SpletnaTrgovinaDiploma
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
             });
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(); 
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
